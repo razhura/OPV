2b83d6a08856453a773a62db7b99309dfc333199
EVAN <134075131+razhura@users.noreply.github.com>
2025-05-20 14:44:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,12 +38,12 @@ def add_bg_from_github():
             width: 100%;
             height: 100%;
             background-color: rgba(255, 255, 255, {st.session_state.opacity});
-            z-index: 0;
+            z-index: -1;
         }}
 
         .main > div {{
             position: relative;
-            z-index: 1;
+            z-index: 0;
         }}
         </style>
 
